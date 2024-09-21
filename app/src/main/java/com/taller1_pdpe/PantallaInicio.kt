37c8dc35ad3256e0ad12ef99f45972827c2f7c76
HugoSanchezGallego <hsancgal@myuax.com>
2024-09-21 21:05:37 +0200

    Añadir fotos dependiendo del tipo de saludo

--- app/src/main/java/com/taller1_pdpe/PantallaInicio.kt
+++ app/src/main/java/com/taller1_pdpe/PantallaInicio.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.taller1_pdpe.ui.theme.Taller1_PDPETheme
@@ -31,6 +33,7 @@ class PantallaInicio : ComponentActivity() {
 @Composable
 fun PantallaInicioContent(onNavigateToMain: () -> Unit) {
     val saludo = obtenerSaludo()
+    val imageResId = obtenerImageResId(saludo)
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -39,6 +42,12 @@ fun PantallaInicioContent(onNavigateToMain: () -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = saludo)
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(id = imageResId),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = onNavigateToMain) {
             Text(text = "Actividad Principal")
@@ -52,6 +61,14 @@ fun obtenerSaludo(): String {
         in 0..11 -> "Buenos días"
         in 12..17 -> "Buenas tardes"
         else -> "Buenas noches"
+    }
+}
+
+fun obtenerImageResId(saludo: String): Int {
+    return when (saludo) {
+        "Buenos días" -> R.drawable.buenos_dias
+        "Buenas tardes" -> R.drawable.buenas_tardes
+        else -> R.drawable.buenas_noches
     }
 }
 
